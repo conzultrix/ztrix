a0ffb1452fdb615f38fac4ed44edeee57797c0f4
Eseosa Aigbogun <eseosa.aigbogun@conzultrix.com>
2015-08-02 04:37:33 +0100

    began building login form page

--- template.php
+++ template.php
@@ -5,3 +5,53 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * Ztrix theme.
  */
+function ztrix_theme() {
+  $items = array();
+  
+  $items['user_login'] = array(
+    'render element' => 'form',
+    'path' => drupal_get_path('theme', 'ztrix') . '/templates',
+    'template' => 'user-login',
+    'preprocess functions' => array(
+      'ztrix_preprocess_user_login',
+    ),
+  );
+  
+  $items['user_registration_form'] = array(
+    'render element' => 'form',
+    'path' => drupal_get_path('theme', 'ztrix') . '/templates',
+    'template' => 'user-registration-form',
+    'preprocess functions' => array(
+      'ztrix_preprocess_user_registration_form',
+    ),
+  );
+  
+  $items['user_pass'] = array(
+    'render element' => 'form',
+    'path' => drupal_get_path('theme', 'ztrix') . '/templates',
+    'template' => 'user-pass',
+    'preprocess functions' => array(
+      'ztrix_preprocess_user_pass',
+    ),
+  );
+  
+  return $items;
+}
+
+/**
+ * Implements hook_form_FORM_ID_alter().
+ */
+function ztrix_form_user_login_alter(&$form, &$form_state) {
+  dpm($form);
+  unset($form['name']['#description']);
+  unset($form['pass']['#description']);
+  $form['actions']['submit']['#value'] = 'Sign In';
+  $form['actions']['submit']['#attributes']['class'][] = 'btn';
+}
+
+/**
+ * Implements hook_page_alter().
+ */
+function ztrix_page_alter(&$page) {
+  dpm($page);
+}
---
+++ templates/user-login.tpl.php
@@ -0,0 +1,15 @@
+<?php
+/*print '<pre>';
+print_r($variables);
+print '</pre>';*/
+//dpm($variables);
+?>
+<p><?php print render($intro_text); ?></p>
+<div class="ztrix-user-login-form-wrapper">
+  <?php //print drupal_render_children($form); ?>
+  <?php print drupal_render($form['name']); ?>
+  <?php print drupal_render($form['pass']); ?>
+  <?php print drupal_render($form['form_build_id']); ?>
+  <?php print drupal_render($form['form_id']); ?>
+  <?php print drupal_render($form['actions']); ?>
+</div>
